088740d51ada1670d225493464ee4a9e8862493b
YAMAZAKI Makoto <makoto1975@gmail.com>
2011-01-15 12:37:11 +0900

    - 470b5dc0f148bdda5ad44cb05a9008bff233f0b5 の修正をとりけしました

--- prepare_for_release.sh
+++ prepare_for_release.sh
@@ -2,7 +2,7 @@
 set -x
 
 # findbugs 関連の記述をコードから取り除く
-find src/ -name \*.java -exec sed -E -e 's/^import edu\.umd\.cs\.findbugs\.annotations\.[^;]*;$//' -e 's/@DefaultAnnotation(NonNull.class)//' -e 's/@CheckForNull ?//' -i "" {} \;
+find src/ -name \*.java -exec sed -e 's/^import edu\.umd\.cs\.findbugs\.annotations\.[^;]*;$//' -e 's/@DefaultAnnotation(NonNull.class)//' -e 's/@CheckForNull//' -i "" {} \;
 
 # findbugs 関連の jar を classpath から取り除く
 sed -e 's/<classpathentry kind="lib" path="libs\/findbugs-[0-9.]*\.jar"\/>//' -e 's/<classpathentry kind="lib" path="libs\/jsr305-[0-9.]*\.jar"\/>//' -i "" .classpath
